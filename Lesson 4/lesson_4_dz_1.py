45e85d78e048be877f79df48a29182acf2a38299
Eugene <evgeshka120@gmail.com>
2021-11-01 13:33:33 +0300

    Практические задания к уроку № 4

---
+++ Lesson 4/lesson_4_dz_1.py
@@ -0,0 +1,133 @@
+# Задание 1
+#
+# "Проанализировать скорость и сложность одного любого алгоритма,
+# разработанных в рамках домашнего задания первых трех уроков
+
+# Алгоритм взят из задания 9, 1 урока
+import random
+import cProfile
+
+
+# Задание 9 "Вводятся три разных числа. Найти, какое из них является средним (больше одного, но меньше другого)".
+
+
+def middle_number(num1, num2, num3):
+    if num3 > num2 > num1 or num1 > num2 > num3:
+        print(f'Средним числом является {num2}')
+    elif num2 > num1 > num3 or num3 > num1 > num2:
+        print(f'Средним числом является {num1}')
+    else:
+        print(f'Средним числом является {num3}')
+
+
+# Сложность предположительно O(n)
+
+cProfile.run('middle_number(5, 1, 6)')
+# Средним числом является 5
+#          5 function calls in 0.000 seconds
+#
+#    Ordered by: standard name
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+#         1    0.000    0.000    0.000    0.000 lesson_4_dz_1.py:14(middle_number)
+#         1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+#         1    0.000    0.000    0.000    0.000 {built-in method builtins.print}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+cProfile.run('middle_number(50, 10, 60)')
+# Средним числом является 50
+#          5 function calls in 0.000 seconds
+#
+#    Ordered by: standard name
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+#         1    0.000    0.000    0.000    0.000 lesson_4_dz_1.py:14(middle_number)
+#         1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+#         1    0.000    0.000    0.000    0.000 {built-in method builtins.print}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+cProfile.run('middle_number(500, 100, 600)')
+# Средним числом является 500
+#          5 function calls in 0.000 seconds
+#
+#    Ordered by: standard name
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+#         1    0.000    0.000    0.000    0.000 lesson_4_dz_1.py:14(middle_number)
+#         1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+#         1    0.000    0.000    0.000    0.000 {built-in method builtins.print}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+cProfile.run('middle_number(5000, 1000, 6000)')
+# Средним числом является 5000
+#          5 function calls in 0.000 seconds
+#
+#    Ordered by: standard name
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+#         1    0.000    0.000    0.000    0.000 lesson_4_dz_1.py:14(middle_number)
+#         1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+#         1    0.000    0.000    0.000    0.000 {built-in method builtins.print}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+
+def new_middle_number(num1, num2, num3):
+    arr = [num1, num2, num3]
+    arr.sort()
+    print(f' Средним числом является {arr[1]}')
+
+# Сложность алгоритма равна O(n * log(n))
+
+cProfile.run('new_middle_number(5, 1, 6)')
+# Средним числом является 5
+#          6 function calls in 0.000 seconds
+#
+#    Ordered by: standard name
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+#         1    0.000    0.000    0.000    0.000 lesson_4_dz_1.py:29(new_middle_number)
+#         1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+#         1    0.000    0.000    0.000    0.000 {built-in method builtins.print}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+#         1    0.000    0.000    0.000    0.000 {method 'sort' of 'list' objects}
+cProfile.run('new_middle_number(50, 10, 60)')
+# Средним числом является 50
+#          6 function calls in 0.000 seconds
+#
+#    Ordered by: standard name
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+#         1    0.000    0.000    0.000    0.000 lesson_4_dz_1.py:29(new_middle_number)
+#         1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+#         1    0.000    0.000    0.000    0.000 {built-in method builtins.print}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+#         1    0.000    0.000    0.000    0.000 {method 'sort' of 'list' objects}
+cProfile.run('new_middle_number(500, 100, 600)')
+# Средним числом является 500
+#          6 function calls in 0.000 seconds
+#
+#    Ordered by: standard name
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+#         1    0.000    0.000    0.000    0.000 lesson_4_dz_1.py:29(new_middle_number)
+#         1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+#         1    0.000    0.000    0.000    0.000 {built-in method builtins.print}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+#         1    0.000    0.000    0.000    0.000 {method 'sort' of 'list' objects}
+cProfile.run('new_middle_number(5000, 1000, 6000)')
+# Средним числом является 5000
+#          6 function calls in 0.000 seconds
+#
+#    Ordered by: standard name
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+#         1    0.000    0.000    0.000    0.000 lesson_4_dz_1.py:29(new_middle_number)
+#         1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+#         1    0.000    0.000    0.000    0.000 {built-in method builtins.print}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+#         1    0.000    0.000    0.000    0.000 {method 'sort' of 'list' objects}
