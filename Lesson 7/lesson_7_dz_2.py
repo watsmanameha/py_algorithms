6d2af09b9f92f47f0ed010e302fd80d944efa206
watsmanameha <62775429+watsmanameha@users.noreply.github.com>
2021-12-29 21:11:01 +0300

    Практические задания к уроку № 7 (#7)

---
+++ Lesson 7/lesson_7_dz_2.py
@@ -0,0 +1,39 @@
+# Задание 2
+#
+# "Отсортируйте по возрастанию методом слияния одномерный вещественный массив,
+# заданный случайными числами на промежутке [0; 50). Выведите на экран исходный и отсортированный массивы".
+import random
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left, right = merge_sort(arr[:mid]), merge_sort(arr[mid:])
+
+    return merge(left, right, arr.copy())
+
+
+def merge(left, right, merged):
+    left_cursor, right_cursor = 0, 0
+
+    while left_cursor < len(left) and right_cursor < len(right):
+
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+
+    return merged
+
+
+print(merge_sort([random.randint(0, 50) for i in range(10)]))
